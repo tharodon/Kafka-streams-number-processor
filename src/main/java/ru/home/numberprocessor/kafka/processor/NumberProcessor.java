dc28ea27fc6f99aa8477dd82dac5c82cd87fead2
rbxMOS003307 <Siebel202306>
2023-10-26 02:37:33 +0300

    add dynamic params to mongoDb

--- src/main/java/ru/home/numberprocessor/kafka/processor/NumberProcessor.java
+++ src/main/java/ru/home/numberprocessor/kafka/processor/NumberProcessor.java
@@ -2,18 +2,23 @@ package ru.home.numberprocessor.kafka.processor;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.state.WindowStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.home.numberprocessor.StateNumber;
+import ru.home.numberprocessor.model.NumberProcessRuleModel;
 import ru.home.numberprocessor.serde.StateNumberSerde;
+import ru.home.numberprocessor.service.NumberProcessorRuleManager;
+import ru.home.numberprocessor.service.StateNumber;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 @Service
@@ -23,27 +28,48 @@ public class NumberProcessor {
     @Value("${spring.kafka.producer.topic}")
     private String topic;
 
-    @Value("${spring.kafka.streams.grouping-time.min}")
-    private Long minutes;
+    private final NumberProcessorRuleManager numberProcessorRuleManager;
+
 
     @Autowired
     public void buildPipeLine(StreamsBuilder builder) {
         KStream<String, String> numberStream = builder.stream(topic);
+        Materialized<String, StateNumber, WindowStore<Bytes, byte[]>> materialized = createMaterialized();
+        AtomicReference<NumberProcessRuleModel> numberProcessRuleModel = new AtomicReference<>(new NumberProcessRuleModel());
         numberStream
-                .peek((key, value) -> log.info("start processing... {}", value))
+                .peek((key, value) -> numberProcessRuleModel.set(numberProcessorRuleManager.extractRule()))
+                .peek((key, value) -> log.info(numberProcessRuleModel.get().getMessage() + " {}", value))//todo change to normally message
                 .groupBy((key, value) -> "")
-                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(minutes)))
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(getTimeOFWindowMinutes(numberProcessRuleModel))))
                 .aggregate(
-                        StateNumber::new,
-                        (key, value, aggregate) -> aggregate.process(Integer.valueOf(value)),
-                        Materialized.with(null, new StateNumberSerde())
+                        this::createStateNumber,
+                        (key, value, aggregate) -> getProcess(value, aggregate),
+                        materialized
                 )
                 .toStream()
                 .mapValues(StateNumber::getAggregatedByTen)
                 .flatMapValues(Map::entrySet)
-                .peek((key, value) -> value.getValue().removeIf(list -> list.size() < 3))
+                .peek((key, value) -> value.getValue().removeIf(list -> list.size() < numberProcessRuleModel.get().getLengthOfGroup()))
                 .filterNot((key, value) -> value.getValue().isEmpty())
-                .foreach(((key, value) -> log.info("groups of ten: {}", value)));
+                .foreach((key, value) -> log.info("groups of ten: {}", value));
+    }
+
+    private Long getTimeOFWindowMinutes(AtomicReference<NumberProcessRuleModel> numberProcessRuleModel) {
+        return numberProcessRuleModel.get().getTimeOFWindowMinutes();
+    }
+
+    private StateNumber createStateNumber() {
+        return new StateNumber();
+    }
+
+    private StateNumber getProcess(String value, StateNumber aggregate) {
+        return aggregate.process(Integer.valueOf(value));
+    }
+
+    private Materialized<String, StateNumber, WindowStore<Bytes, byte[]>> createMaterialized() {
+        return Materialized.<String, StateNumber, WindowStore<Bytes, byte[]>>as("random-number-store")
+                .withKeySerde(null)
+                .withValueSerde(new StateNumberSerde());
     }
 
 }
